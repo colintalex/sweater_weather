bd88baae87ef96e1e0915db81afdf98547f938be
Colin Alexander <colintalex@gmail.com>
2020-06-08 10:59:58 -0600

    Updates test

--- spec/api/v1/foodie/foodie_request_spec.rb
+++ spec/api/v1/foodie/foodie_request_spec.rb
@@ -5,16 +5,15 @@ describe "Food Search Query" do
     get  '/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian'
 
     expect(response).to be_successful
-    require "pry"; binding.pry
-    json = JSON.parse(response.body, symbolize_headers: true)
-    
-    expect(json[:data]).to be_a(Hash)
-    expect(json[:data][:type]).to eql('foodie')
-    expect(jason[:data][:attributes]).to be_a(Hash)
-    expect(jason[:data][:attributes][:end_location]).to be_a(String)
-    expect(jason[:data][:attributes][:travel_time]).to be_a(String)
-    expect(jason[:data][:attributes][:forecast]).to be_a(Hash)
-    expect(jason[:data][:attributes][:restaurant]).to be_a(Hash)
+    json = JSON.parse(response.body, symbolize_name:true)
+
+    expect(json['data']).to be_a(Hash)
+    expect(json['data']['type']).to eql('foodie')
+    expect(json['data']['attributes']).to be_a(Hash)
+    expect(json['data']['attributes']['end_location']).to be_a(String)
+    expect(json['data']['attributes']['travel_time']).to be_a(String)
+    expect(json['data']['attributes']['forecast']).to be_a(Hash)
+    expect(json['data']['attributes']['restaurant']).to be_a(Hash)
 
   end
 end
