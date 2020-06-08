f092f6c4a8855541dfb8b17a623e3b1c3837d065
Colin Alexander <colintalex@gmail.com>
2020-06-08 09:22:13 -0600

    Adds foodie route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
-      get 'backgrounds', to: 'images#show'
+      get '/backgrounds', to: 'images#show'
+      get '/foodie', to: 'foodie#show'
     end
   end
 
