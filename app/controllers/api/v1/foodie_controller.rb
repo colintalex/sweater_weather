7df184309d61c57715b322d31f98ec8d142d7798
Colin Alexander <colintalex@gmail.com>
2020-06-08 10:14:12 -0600

    Updates Foodie Controller

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FoodieController < ApplicationController
   def show
-    
+    render json: FoodieSerializer.new(foodie(foodie_params))
   end
 
   private
@@ -9,7 +9,22 @@ class Api::V1::FoodieController < ApplicationController
     params.permit(:start, :end, :search)
   end
 
-  def get_travel_time(data)
+  def foodie(data)
+    info = get_travel_info(data)
+    new_data = {}
+    new_data[:travel_time] = info['distance']['text']
+    new_data[:end_location] = data[:end]
+    new_data[:forecast] = get_forecast(info['end_location'])
+  end
+
+  def get_travel_info(data)
     response = GoogleGeoService.new.travel_time(data[:start], data[:end])
+    json = JSON.parse(response.body, symbolize_headers: true)
+    json['routes'].first['legs'].first
+  end
+
+  def get_forecast(coordinates)
+    response = OpenWeatherService.new.get_forecast(coordinates)
+    response['current']['weather'].first['description']
   end
 end
