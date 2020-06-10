569c8387d9e5492fcee38f3be9a94657c4effa7e
Colin Alexander <colintalex@gmail.com>
2020-06-09 23:56:23 -0600

    Creates error handling for bad creds/ no username.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,29 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       render json: UsersSerializer.new(@user)
     else
-      require "pry"; binding.pry
+      @error = auth_error(@user)
+      render json: ErrorSerializer.new(@error), status: :unauthorized
     end
   end
 
+  private
+
+  def auth_error(user)
+    if user.nil?
+      Error.new(no_user_error)
+    else
+      Error.new(bad_password_error)
+    end
+  end
+
+  def bad_password_error
+    'Incorrect password, please try again'
+  end
+
+  def no_user_error
+    'Email is not registered. Please sign-up for access.'
+  end
 end
