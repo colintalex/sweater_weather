9dfceefea572a51a5b30ee72581d1fab81fbda1b
Colin Alexander <colintalex@gmail.com>
2020-06-09 23:56:48 -0600

    Updates to handle single errors/strings

--- app/poros/error.rb
+++ app/poros/error.rb
@@ -2,6 +2,14 @@ class Error
   attr_reader :description
 
   def initialize(errors)
-    @description = errors.uniq.to_sentence
+    @description = construct_msg(errors)
+  end
+
+  def construct_msg(errors)
+    if errors.kind_of?(Array)
+      errors.uniq.to_sentence
+    else
+      errors
+    end
   end
 end
